4e337d75f67dc437609d58ab7d68c13ae84879dc
venkatsgithub1 <venkatsgithub@gmail.com>
2017-08-26 23:38:02 +0530

    Create boolean_example.go
    If we don't provide value to boolean it's false, integer it's 0, string it's empty string.

---
+++ src/booleans/boolean_example.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sunsetsinthewest := true
+	fmt.Printf("sun sets in the west:%t\n", sunsetsinthewest)
+	waterissolid := !true
+	fmt.Printf("water is solid:%t\n", waterissolid)
+	var circlesarerectangles bool
+	fmt.Printf("Circles are rectangles:%t\n", circlesarerectangles)
+}
