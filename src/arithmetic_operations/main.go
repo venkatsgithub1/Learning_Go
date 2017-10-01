1c6b802950897144887b0fa35071b56a5c951df2
venkatsgithub1 <venkatsgithub@gmail.com>
2017-10-01 18:05:34 +0530

    Few examples on arithmetic operators.

---
+++ src/arithmetic_operations/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	add := 1 + 2
+	fmt.Println(add)
+
+	subtract := 6 - 3
+	fmt.Println(subtract)
+
+	remainder := 12 % 4
+	fmt.Println(remainder)
+
+	// increment operator
+	var1 := 1
+	var1++
+	fmt.Println(var1)
+
+	// decrement operator
+	var1--
+	fmt.Println(var1)
+
+	var1 += 10
+	fmt.Println(var1)
+}
